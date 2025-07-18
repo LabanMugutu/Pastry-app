fd2b04fa4475d408aaafcd1eed6c091ed269ec36
LabanMugutu <labanmugutu@gmail.com>
2025-07-18 20:30:57 +0300

    Fetches  recipes

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const baseURL = "http://localhost:3000/recipes";
 const recipeList = document.getElementById("recipe-list");
 const searchInput = document.getElementById("search");
 const filterSelect = document.getElementById("filter");
+function fetchRecipes() {
+  fetch(baseURL)
+    .then(res => res.json())       // Convert response to JSON
+    .then(data => {
+      displayRecipes(data);        // Pass recipes to render function
+    });
+}
