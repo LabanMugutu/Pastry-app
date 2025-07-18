ab132ebab57baad92566b77b5c15350fe7a10f4a
LabanMugutu <labanmugutu@gmail.com>
2025-07-18 20:29:16 +0300

    References HTML elements

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 // Base URL of json-server
 const baseURL = "http://localhost:3000/recipes";
+// Get references to HTML elements
+const recipeList = document.getElementById("recipe-list");
+const searchInput = document.getElementById("search");
+const filterSelect = document.getElementById("filter");
